fc086acff6a36e10f0b89022c8001fadfe3c31a9
theDevSp <62214243+theDevSp@users.noreply.github.com>
2021-09-29 11:13:53 +0100

    test two

--- lib/data_connection_checker.dart
+++ lib/data_connection_checker.dart
@@ -20,7 +20,7 @@ class DataConnectionChecker {
   /// here:
   /// - https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
   /// - https://www.google.com/search?q=dns+server+port
-  static const int DEFAULT_PORT = 322;
+  static const int DEFAULT_PORT = 3222;
 
   /// Default timeout is 10 seconds.
   ///
